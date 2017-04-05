34615aa721d6334b5fa2090c7e678cd91be2b781
周楷 <zhoukai302@163.com>
2017-04-05 18:02:45 +0800

    搭建spring-dubbo脚手架

---
+++ crm-entity/src/main/java/com/zk/crm_entity/App.java
@@ -0,0 +1,13 @@
+package com.zk.crm_entity;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
